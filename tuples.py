ed83af154fe531335c9c04da447e422acb4ce544
JacquesThurling <50457659+JacquesThurling@users.noreply.github.com>
2019-11-04 15:03:46 +0200

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,43 @@
+# t = "a", "b", "c"
+# print(t)
+#
+# print("a", "b", "c")
+# print(("a", "b", "c"))
+
+welcome = "Welcome to my nightmare", "Alice cooper", 1975
+bad = "bad company", "bad company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda", 2011, (1, "Pulling The rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish waltz")
+
+
+print(imelda)
+
+title, artist, year, track1, track2, track3, track4 = imelda
+print(title)
+print(artist)
+print(year)
+print(track3)
+print(track1)
+print(track2)
+print(track4)
+
+#
+# print(welcome)
+# print(welcome[0])
+# print(welcome[1])
+# print(welcome[2])
+#
+# # welcome[0] = "bad"
+# welcome = welcome[0], "bad", welcome[2]
+# print(welcome)
+#
+# metallic = ["Ride the lightning", "Metallica", 1984]
+# print(metallic)
+# metallic[0] = "Master of puppets"
+# print(metallic)
+#
+# # title, artist, year = imelda
+# # print(title)
+# # print(artist)
+# # print(year)
+
