84469084e3c671cd842b9725a5fa137e1925643b
JacquesThurling <50457659+JacquesThurling@users.noreply.github.com>
2019-11-04 15:04:00 +0200

    Create WhileLoopChallengeSolution.py

---
+++ WhileLoopChallengeSolution.py
@@ -0,0 +1,20 @@
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+
+print("Please guess a number between 1 and {}: ".format(highest))
+guess = 0
+
+while guess != answer:
+    guess = int(input())
+
+    if guess == 0:
+        print("bye bye")
+        break
+    if guess < answer:
+        print("Please guess higher")
+    elif guess > answer:
+        print("Please guess lower")
+    else:
+        print("Well done, you guessed it")
