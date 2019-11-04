692eea1c972a7653048fbc624c1d425ba469d490
JacquesThurling <50457659+JacquesThurling@users.noreply.github.com>
2019-11-04 15:03:13 +0200

    Create l.py

---
+++ l.py
@@ -0,0 +1,56 @@
+# shopping_list = ["milk", "pasta", "eggs"]
+# for item in shopping_list:
+#     if item == 'pasta':
+#         print("I'm ignoring " + item)
+#         break
+#
+#     print("Buy " + item)
+
+# meal = ["egg", "bacon", "milk"]
+#
+# nasty_food_item = ''
+# nastyFoodItem = ''
+#
+# for item in meal:
+#     if item == 'bacon':
+#         nasty_food_item = item
+#         break
+# else:
+#     print("I'll have a plate of that please")
+#
+# if nasty_food_item:
+#     print("Can't i have anything without spam in it")
+#
+
+# for i in range(20):
+#     if i % 3 != 0 and i % 5 != 0:
+#         print(i)
+
+# for x in range(20):
+#     if x == 0 or x == 9:
+#         continue
+#     print(x)
+#
+# else:
+#     print("I like ike")
+#
+# tea = 34
+#
+# if tea % 2 == 0 and tea == 34:
+#     print("I hate you")
+# else:
+#     print("Why?")
+# name = input("Who are you? ")
+#
+# enter = input("What is your statement {0}? ".format(name))
+# letter = input("Enter the letter you want to check?")
+#
+# total = ''
+# count = 0
+#
+# for char in enter:
+#     if char in letter:
+#         print(enter)
+#         continue
+#
+# print("There are {0} {1}'s".format(count, total))
